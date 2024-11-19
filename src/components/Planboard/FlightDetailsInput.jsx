a84430f2b85cf439477c2df2689c1d0319d62d4e
candylxy95 <lee.ydnac@gmail.com>
2024-11-19 15:09:46 +0800

    resolved conflicts

--- src/components/Planboard/FlightDetailsInput.jsx
+++ src/components/Planboard/FlightDetailsInput.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import styles from "./PlanBoard.module.css";
 import { FlightContext } from "../context/FlightContext";
+import LoadingSpinner from "./LoadingSpinner";
 
 const FlightDetailsInput = () => {
   const [origin, setOrigin] = useState("");
@@ -10,11 +11,14 @@ const FlightDetailsInput = () => {
   const [cabinClass, setCabinClass] = useState("ECONOMY");
   const { setDepartureFlightData, setArrivalFlightData } =
     useContext(FlightContext);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    console.log("Form submitted"); // Check if the form submission is detected
     const departureUrl = `http://localhost:5001/WanderGoWhere/flights?origin=${origin}&destination=${destination}&departureDate=${departureDate}&cabinClass=${cabinClass}`;
     const arrivalUrl = `http://localhost:5001/WanderGoWhere/flights?origin=${destination}&destination=${origin}&departureDate=${returnDate}&cabinClass=${cabinClass}`;
+    setIsLoading(true);
     try {
       // Fetch both departure and arrival data in parallel
       const [departureResponse, arrivalResponse] = await Promise.all([
@@ -36,57 +40,55 @@ const FlightDetailsInput = () => {
 
       const departureFlightData = await departureResponse.json();
       const arrivalFlightData = await arrivalResponse.json();
-
       setDepartureFlightData(departureFlightData);
-
       setArrivalFlightData(arrivalFlightData);
+      setIsLoading(false);
     } catch (error) {
       console.error("Error fetching flight data:", error);
+      setIsLoading(false);
     }
   };
 
   return (
     <div className={styles.flightdetailsinput}>
       <h1>Flight Search</h1>
-      <form className={styles.flightform} onSubmit={handleSubmit}>
-        <div className={styles.flightformcity}>
-          <div>
-            <label>Origin:</label>
-            <input
-              type="text"
-              value={origin}
-              onChange={(e) => setOrigin(e.target.value)}
-              placeholder="Enter origin airport"
-            />
-          </div>
-          <div>
-            <label>Destination:</label>
-            <input
-              type="text"
-              value={destination}
-              onChange={(e) => setDestination(e.target.value)}
-              placeholder="Enter destination airport"
-            />
-          </div>
-
-          <div>
-            <label>Departure Date:</label>
-            <input
-              type="date"
-              value={departureDate}
-              onChange={(e) => setDepartureDate(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Return Date:</label>
-            <input
-              type="date"
-              value={returnDate}
-              onChange={(e) => setReturnDate(e.target.value)}
-            />
-          </div>
+      {isLoading ? <LoadingSpinner /> : null}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Origin:</label>
+          <input
+            type="text"
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+            placeholder="Enter origin airport"
+          />
         </div>
-        <div className={styles.cabinselection}>
+        <div>
+          <label>Destination:</label>
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Enter destination airport"
+          />
+        </div>
+        <div>
+          <label>Departure Date:</label>
+          <input
+            type="date"
+            value={departureDate}
+            onChange={(e) => setDepartureDate(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Return Date:</label>
+          <input
+            type="date"
+            value={returnDate}
+            onChange={(e) => setReturnDate(e.target.value)}
+          />
+        </div>
+        <div>
           <label>Cabin Class:</label>
           <select
             value={cabinClass}
@@ -96,17 +98,10 @@ const FlightDetailsInput = () => {
             <option value="BUSINESS">Business</option>
           </select>
         </div>
-        <button
-          style={{
-            borderRadius: "0 0 20px 20px",
-            boxShadow: "0 8px 18px rgba(0, 0, 0, 0.6)",
-          }}
-          type="submit"
-        >
-          Search Flights
-        </button>
+        <button type="submit">Search Flights</button>
       </form>
     </div>
   );
 };
+
 export default FlightDetailsInput;
